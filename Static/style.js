68e658420090fd2f598cbbcbd0413146fb99ebb8
Laney Humble <laneyhumble@Laneys-MBP.etown.edu>
2023-10-19 16:21:11 -0400

    updated html

---
+++ Static/style.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+    window.addEventListener("load", init);
+
+    function init() {
+        $('#navigator').load('efd.html');
+        $('#navigator').load('ge.html');
+
+    }
+})
